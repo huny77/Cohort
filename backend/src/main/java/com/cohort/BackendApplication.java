895bae11e027c35517730255ff92844705719cd5
Su-bro <tngud941208@naver.com>
2021-10-28 19:22:33 +0900

    TEST

--- backend/src/main/java/com/cohort/BackendApplication.java
+++ backend/src/main/java/com/cohort/BackendApplication.java
@@ -1,7 +1,6 @@
 package com.cohort;
 
 import java.nio.charset.Charset;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
