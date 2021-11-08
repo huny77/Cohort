62c287e21d7cb27c408d2b2b276892891624bf2e
한상정 <hsj7630@daum.net>
2021-11-08 15:43:38 +0900

    feat: Add googlelogin

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,44 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import { CookiesProvider } from 'react-cookie';
+import { tempSetUser, check } from './modules/user';
+import { getCookie } from './lib/cookie';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+function loadUser() {
+  try {
+    const mail = getCookie('mail');
+    if (!mail) return; // 로그인 상태가 아니라면 아무것도 안 함
+
+    store.dispatch(tempSetUser(mail));
+    store.dispatch(check(mail));
+  } catch (e) {
+    console.log('cookie is not working');
+  }
+}
+
+sagaMiddleware.run(rootSaga);
+loadUser();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </CookiesProvider>,
   document.getElementById('root'),
 );
 
