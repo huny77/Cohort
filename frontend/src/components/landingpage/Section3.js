41704966b66dd3b0b914ccf31d5d8b57c6e8eaa7
한상정 <hsj7630@daum.net>
2021-11-11 11:00:06 +0900

    feat: Update user

--- frontend/src/components/landingpage/Section3.js
+++ frontend/src/components/landingpage/Section3.js
@@ -1,4 +1,13 @@
-import { Typography, Box, Grid, Card, CardActionArea, CardMedia, CardContent, Button } from '@mui/material';
+import {
+  Typography,
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Button,
+} from '@mui/material';
 import NearMeIcon from '@mui/icons-material/NearMe';
 import styled from 'styled-components';
 import { useInView } from 'react-intersection-observer';
@@ -7,7 +16,7 @@ const ZoomBox = styled(Box)`
   display: flex;
   min-height: 100vh;
   background-size: cover;
-  background-color: #F2F3F7;
+  background-color: #f2f3f7;
   font-family: Nunito;
   opacity: ${(props) => (props.inView ? 1 : 0)};
   transform: ${(props) => (props.inView ? `scale(100%)` : `scale(85%)`)};
@@ -23,12 +32,12 @@ const renderContent = () => {
   return (
     <>
       <Box sx={{ margin: 'auto' }}>
-      <CustomizedText sx={{marginBottom: 1, fontWeight: 100}}>
-        해당 알고리즘 사이트에서 어려운 문제들을 스터디하세요.
-      </CustomizedText>
-      <CustomizedText sx={{marginBottom: 10}}>
-        5개의 언어를 활용해 알고리즘 문제를 해결할 수 있습니다.
-      </CustomizedText>
+        <CustomizedText sx={{ marginBottom: 1, fontWeight: 100 }}>
+          해당 알고리즘 사이트에서 어려운 문제들을 스터디하세요.
+        </CustomizedText>
+        <CustomizedText sx={{ marginBottom: 10 }}>
+          5개의 언어를 활용해 알고리즘 문제를 해결할 수 있습니다.
+        </CustomizedText>
         <Grid container spacing={8}>
           <Grid item xs={4}>
             <Card sx={{ maxWidth: 345 }}>
@@ -40,13 +49,29 @@ const renderContent = () => {
                   alt="baejoon"
                 />
                 <CardContent>
-                  <Box sx={{ display: 'flex', justifyContent: 'space-around', p: 1, m: 1 }}>
-                  <Typography gutterBottom variant="h5" component="div" margin="auto">
-                    백준 온라인 저지
-                  </Typography>
-                  <Button endIcon={<NearMeIcon />} href="https://www.acmicpc.net/">
-                    이동
-                  </Button>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-around',
+                      p: 1,
+                      m: 1,
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      margin="auto"
+                    >
+                      백준 온라인 저지
+                    </Typography>
+                    <Button
+                      endIcon={<NearMeIcon />}
+                      href="https://www.acmicpc.net/"
+                      target="_blank"
+                    >
+                      이동
+                    </Button>
                   </Box>
                 </CardContent>
               </CardActionArea>
@@ -58,17 +83,35 @@ const renderContent = () => {
                 <CardMedia
                   component="img"
                   height="200"
-                  image={`${process.env.PUBLIC_URL + '/assets/programmers.jpg'}`}
+                  image={`${
+                    process.env.PUBLIC_URL + '/assets/programmers.jpg'
+                  }`}
                   alt="programmers"
                 />
                 <CardContent>
-                  <Box sx={{ display: 'flex', justifyContent: 'space-around', p: 1, m: 1 }}>
-                  <Typography gutterBottom variant="h5" component="div" margin="auto">
-                    프로그래머스
-                  </Typography>
-                  <Button endIcon={<NearMeIcon />} href="https://programmers.co.kr/">
-                    이동
-                  </Button>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-around',
+                      p: 1,
+                      m: 1,
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      margin="auto"
+                    >
+                      프로그래머스
+                    </Typography>
+                    <Button
+                      endIcon={<NearMeIcon />}
+                      href="https://programmers.co.kr/"
+                      target="_blank"
+                    >
+                      이동
+                    </Button>
                   </Box>
                 </CardContent>
               </CardActionArea>
@@ -84,13 +127,29 @@ const renderContent = () => {
                   alt="goorm"
                 />
                 <CardContent>
-                  <Box sx={{ display: 'flex', justifyContent: 'space-around', p: 1, m: 1 }}>
-                  <Typography gutterBottom variant="h5" component="div" margin="auto">
-                    구름 레벨
-                  </Typography>
-                  <Button endIcon={<NearMeIcon />} href="https://level.goorm.io/">
-                    이동
-                  </Button>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-around',
+                      p: 1,
+                      m: 1,
+                    }}
+                  >
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      margin="auto"
+                    >
+                      구름 레벨
+                    </Typography>
+                    <Button
+                      endIcon={<NearMeIcon />}
+                      href="https://level.goorm.io/"
+                      target="_blank"
+                    >
+                      이동
+                    </Button>
                   </Box>
                 </CardContent>
               </CardActionArea>
