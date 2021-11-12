2574cf8a3aa479f9c22391cb6d6b5cd4ac124132
한상정 <hsj7630@daum.net>
2021-11-12 11:29:13 +0900

    feat: Update pagination

--- frontend/src/pages/PostListPage.js
+++ frontend/src/pages/PostListPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import PostList from '../components/posts/PostList';
 import PaginationPostList from '../components/posts/PaginationPostList';
+import PaginationPostList2 from '../components/posts/PaginationPostList2';
 
 const PostListContainer = styled.div`
   max-width: 80%;
@@ -16,7 +17,7 @@ const PostListPage = () => {
       <PostListContainer>
         <PostList />
       </PostListContainer>
-      <PaginationPostList />
+      <PaginationPostList2 />
     </>
   );
 };
