f6d73f510b7df4e17692e9881118556c4f4ae1d9
한상정 <hsj7630@daum.net>
2021-11-17 22:55:44 +0900

    feat: Add helmet

--- frontend/src/pages/PostPage.js
+++ frontend/src/pages/PostPage.js
@@ -1,6 +1,7 @@
 import PostViewer from '../components/post/PostViewer';
 import styled from 'styled-components';
 import Comment from '../components/post/Comment';
+import { Helmet } from 'react-helmet-async';
 
 const StyledDiv = styled.div`
   max-width: 80%;
@@ -10,11 +11,16 @@ const StyledDiv = styled.div`
 
 const PostPage = () => {
   return (
-    <StyledDiv>
-      <div style={{ height: '6rem' }}></div>
-      <PostViewer />
-      <Comment />
-    </StyledDiv>
+    <>
+      <Helmet>
+        <title>Cohort - 게시판</title>
+      </Helmet>
+      <StyledDiv>
+        <div style={{ height: '6rem' }}></div>
+        <PostViewer />
+        <Comment />
+      </StyledDiv>
+    </>
   );
 };
 
--- frontend/src/pages/WritePage.js
+++ frontend/src/pages/WritePage.js
@@ -4,6 +4,7 @@ import CodeEditor from '../components/post/CodeEditor';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import CodeResult from '../components/post/CodeResult';
+import { Helmet } from 'react-helmet-async';
 
 const PostContainer = styled.div`
   display: flex;
@@ -24,13 +25,18 @@ const WritePage = () => {
   }
 
   return (
-    <div>
-      <div style={{ height: '4rem' }}></div>
-      <PostContainer>
-        <CodeEditor />
-        <CodeResult />
-      </PostContainer>
-    </div>
+    <>
+      <Helmet>
+        <title>Cohort - 코드 작성</title>
+      </Helmet>
+      <div>
+        <div style={{ height: '4rem' }}></div>
+        <PostContainer>
+          <CodeEditor />
+          <CodeResult />
+        </PostContainer>
+      </div>
+    </>
   );
 };
 
